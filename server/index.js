f61e8ff0c62404329b569ad973db45e90fa43091
dahalapaar <contact.apaardahal@gmail.com>
2023-02-09 11:39:17 +0545

    Backend completed

--- server/index.js
+++ server/index.js
@@ -4,11 +4,17 @@ import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
 
+import postRoutes from './routes/postRoutes.js';
+import apaaraiRoutes from './routes/apaaraiRoutes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb'}));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/apaar', apaaraiRoutes);
 
 app.get('/', async (req, res) => {
     res.send('Hello from ApaarAI');
@@ -27,3 +33,4 @@ const startServer = async () => {
 }
 
 startServer();
+
